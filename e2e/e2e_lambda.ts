41d29fed741c3c80d7990d1d26580c488891dea7
if1live <libsora25@gmail.com>
2023-12-12 23:29:16 +0900

    lambda 정보 읽는 함수 추가

--- e2e/e2e_lambda.ts
+++ e2e/e2e_lambda.ts
@@ -5,6 +5,9 @@ import {
   InvocationType,
   InvokeCommand,
   InvokeCommandOutput,
+  ListEventSourceMappingsCommand,
+  ListFunctionsCommand,
+  ListFunctionUrlConfigsCommand,
 } from "@aws-sdk/client-lambda";
 import { Context } from "aws-lambda";
 import { FunctionDefinition, standalone } from "../src/index.js";
@@ -51,6 +54,29 @@ export const definitions: FunctionDefinition[] = [
     handler: lambda_exception,
     events: [],
   },
+  {
+    name: "lambda_event",
+    handler: lambda_simple,
+    events: [
+      {
+        sqs: {
+          queueName: "hello-queue",
+          batchSize: 2,
+        },
+      },
+      {
+        sqs: {
+          queueName: "world-queue",
+          batchSize: 3,
+        },
+      },
+      {
+        httpApi: {
+          route: "POST /foo",
+        },
+      },
+    ],
+  },
 ];
 
 const inst = standalone({
@@ -64,6 +90,7 @@ describe("lambda", () => {
 
   const functionName_echo = "lambda_simple";
   const functionName_exception = "lambda_exception";
+  const functionName_event = "lambda_event";
 
   function extractPayload(output: InvokeCommandOutput) {
     const payloadText = output.Payload
@@ -75,7 +102,7 @@ describe("lambda", () => {
 
   beforeEach(() => invokedSet.clear());
 
-  describe("request-response", () => {
+  describe("invoke: request-response", () => {
     const label = "request-response";
     const input = { label };
     const invocationType = InvocationType.RequestResponse;
@@ -108,7 +135,7 @@ describe("lambda", () => {
     });
   });
 
-  describe("event", () => {
+  describe("invoke: event", () => {
     const label = "event";
     const input = { label };
     const invocationType = InvocationType.Event;
@@ -141,6 +168,106 @@ describe("lambda", () => {
 
       assert.equal(output.StatusCode, 200);
       assert.deepEqual(payload, {});
+    });
+  });
+
+  describe("listFunctions", () => {
+    it("ok", async () => {
+      const output = await client.send(new ListFunctionsCommand({}));
+
+      const list = output.Functions ?? [];
+      assert.equal(list.length, definitions.length);
+
+      const [first, _rest] = list;
+
+      assert.ok(first);
+      assert.equal(first.FunctionName, functionName_echo);
+      assert.equal(
+        first.FunctionArn,
+        "arn:aws:lambda:ap-northeast-1:123456789012:function:lambda_simple",
+      );
+    });
+  });
+
+  describe("listFunctionUrlConfigs", () => {
+    it("function exists + with httpApi", async () => {
+      const output = await client.send(
+        new ListFunctionUrlConfigsCommand({
+          FunctionName: functionName_event,
+        }),
+      );
+      const list = output.FunctionUrlConfigs ?? [];
+      assert.equal(list.length, 1);
+
+      const [first, _rest] = list;
+      assert.ok(first);
+
+      console.log(first);
+
+      assert.equal(
+        first.FunctionArn,
+        "arn:aws:lambda:ap-northeast-1:123456789012:function:lambda_event",
+      );
+      assert.ok(typeof first.FunctionUrl == "string");
+    });
+
+    it("function exists + no httpApi", async () => {
+      const output = await client.send(
+        new ListFunctionUrlConfigsCommand({
+          FunctionName: functionName_echo,
+        }),
+      );
+      const list = output.FunctionUrlConfigs ?? [];
+      assert.equal(list.length, 0);
+    });
+  });
+
+  describe("listEventSourceMappings", () => {
+    it("function exists + with sqs", async () => {
+      const output = await client.send(
+        new ListEventSourceMappingsCommand({
+          FunctionName: functionName_event,
+        }),
+      );
+      const list = output.EventSourceMappings ?? [];
+      assert.equal(list.length, 2);
+
+      const [first, _rest] = list;
+      assert.ok(first);
+
+      assert.equal(first.BatchSize, 2);
+      assert.equal(
+        first.EventSourceArn,
+        "arn:aws:sqs:ap-northeast-1:123456789012:hello-queue",
+      );
+      assert.equal(
+        first.FunctionArn,
+        "arn:aws:lambda:ap-northeast-1:123456789012:function:lambda_event",
+      );
+      assert.equal(first.State, "Enabled");
+    });
+
+    it("function exists + no sqs", async () => {
+      const output = await client.send(
+        new ListEventSourceMappingsCommand({
+          FunctionName: functionName_echo,
+        }),
+      );
+      const list = output.EventSourceMappings ?? [];
+      assert.equal(list.length, 0);
+    });
+
+    it("no exists", async () => {
+      try {
+        const output = await client.send(
+          new ListEventSourceMappingsCommand({
+            FunctionName: "unknown",
+          }),
+        );
+        assert.fail("should not reach");
+      } catch (e) {
+        assert.ok(true);
+      }
     });
   });
 });
--- test/lambda/index.test.ts
+++ test/lambda/index.test.ts
@@ -3,8 +3,10 @@ import http from "node:http";
 import { parseRequest } from "../../src/lambda/index.js";
 
 describe("parse", () => {
+  type Req = Pick<http.IncomingMessage, "method" | "url" | "headers">;
+
   it("invoke", () => {
-    const req: Pick<http.IncomingMessage, "method" | "url" | "headers"> = {
+    const req: Req = {
       method: "POST",
       url: "/2015-03-31/functions/toki-example-dev-sqsMain-01/invocations",
       headers: {
@@ -14,6 +16,47 @@ describe("parse", () => {
     const actual = parseRequest(req);
 
     assert.equal(actual?._tag, "invoke");
-    assert.equal(actual?.functionName, "toki-example-dev-sqsMain-01");
+    if (actual._tag === "invoke") {
+      assert.equal(actual.functionName, "toki-example-dev-sqsMain-01");
+    }
+  });
+
+  it("list", () => {
+    const req: Req = {
+      method: "GET",
+      url: "/2015-03-31/functions",
+      headers: {},
+    };
+    const actual = parseRequest(req);
+
+    assert.equal(actual?._tag, "list");
+  });
+
+  it("urls", () => {
+    const req: Req = {
+      method: "GET",
+      url: "/2021-10-31/functions/lambda_simple/urls",
+      headers: {},
+    };
+    const actual = parseRequest(req);
+
+    assert.equal(actual?._tag, "urls");
+    if (actual._tag === "urls") {
+      assert.equal(actual.functionName, "lambda_simple");
+    }
+  });
+
+  it("event source mappings", () => {
+    const req: Req = {
+      method: "GET",
+      url: "/2015-03-31/event-source-mappings?FunctionName=lambda_simple",
+      headers: {},
+    };
+    const actual = parseRequest(req);
+
+    assert.equal(actual?._tag, "eventSourceMappings");
+    if (actual._tag === "eventSourceMappings") {
+      assert.equal(actual.functionName, "lambda_simple");
+    }
   });
 });
